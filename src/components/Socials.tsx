e7cb4fc73a4523989501e586cb157f22e12708f5
karisDev <karjaakirill@gmail.com>
2023-09-30 21:03:59 +0300

    first commit

---
+++ src/components/Socials.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Socials = () => {
+  return (
+    <SocialContainer>Socials</SocialContainer>
+  )
+}
+
+const SocialContainer = styled.div`
+  color: red;
+`
